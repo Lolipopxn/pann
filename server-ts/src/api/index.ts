799157507012399482702bc557975c369e0c8efd
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-31 15:02:01 +0700

    เพิ่ม api(announcement และ userResult) โดยการ Mock

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,10 +1,11 @@
 import router from 'koa-router'
+import announcement from './announcement'
+import userResult from './user_result'
 
 
 const apiRouter = new router()
 
-apiRouter.get('/api/greet', (ctx, next) => {
-    ctx.body = {msg: 'Server test' }
-})
+apiRouter.use('/api/announcement',announcement.routes())
+apiRouter.use('/api/userResult', userResult.routes())
 
 export default apiRouter
