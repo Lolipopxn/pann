ffc4f99ab6f22a0fcb59bb7799b2adea36053943
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2023-01-19 16:17:14 +0700

    ให้มีการ auth user ก่อนเข้า api ใน api/index.ts

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,11 +1,12 @@
 import router from 'koa-router'
 import announcement from './announcement'
 import userResult from './user_result'
+import { authMiddleware } from '../auth'
 
 
 const apiRouter = new router()
 
-apiRouter.use('/api/announcement',announcement.routes())
-apiRouter.use('/api/userResult', userResult.routes())
+apiRouter.use('/api/announcement',authMiddleware,announcement.routes())
+apiRouter.use('/api/userResult',authMiddleware,userResult.routes())
 
 export default apiRouter
