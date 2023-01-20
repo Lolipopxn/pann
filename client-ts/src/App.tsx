db26c2b502369bfa2496b89e8be2a6c4d78d9bcc
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2023-01-20 20:03:06 +0700

    ให้ app.tsx มีการใช้ auth และ routes

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,61 +1,18 @@
-import Repo from './repositories'
-import { useEffect ,useState } from 'react';
-import Announcement from './models/Announcement';
-import UserResult from './models/UserResult';
+import './App.css'
+import { AuthProvider } from 'react-oidc-context';
+import { oidcConfig } from './config';
+import AppProvider from './AppProvider';
+import AppRoutes from './config/routes';
 
 
 function App() {
-  const [announcementList, setAnnouncementList] = useState<Announcement[]>([])
-  const [userResultList, setuserResultList] = useState<UserResult[]>([])
-
-  const fetchannouncementList = async () => {
-    const result = await Repo.announcements.getAll()
-    if(result){
-      setAnnouncementList(result)
-    }
-  }
-
-  const fetchuserResultList = async () => {
-    const result = await Repo.userResults.getAll()
-    if(result){
-      setuserResultList(result)
-    }
-  }
-
-
-  useEffect(() => {
-    fetchannouncementList()
-    fetchuserResultList()
-  })
-
   return (
-    <div>
-      <hr />
-      {announcementList.map(announcement => (
-        <div key={announcement.id}>
-          <p>ID : {announcement.id}</p>
-          <p>Description : {announcement.description}</p>
-          <p>Positive : {announcement.remarkIfPositive}</p>
-          <p>Negative : {announcement.remarkIfNegative}</p>
-          <p>Time : {announcement.pubDateTime.toLocaleString('en-US')}</p>
-          <p>UserCode : {announcement.userCode}</p>
-        </div>
-      ))}
-      <hr />
-      <div>
-        {userResultList.map(userResult => (
-          <div key={userResult.id}>
-            <p>ID : {userResult.id}</p>
-            <p>Topic : {userResult.announcement.topic}</p>
-            <p>Result : {userResult.result}</p>
-            <p>UserCode : {userResult.userCode}</p>
-          </div>
-        ))}
-      </div>
-      <hr />
-    </div>
-
-  );
+    <AuthProvider {...oidcConfig}>
+      <AppProvider>
+        <AppRoutes/>
+      </AppProvider>
+    </AuthProvider>
+  )
 }
 
 export default App;
